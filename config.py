d0050bcbf6c225afd7010886b97e6f3dbb84b9da
opefeni <80990983+opefeni@users.noreply.github.com>
2022-04-25 12:59:16 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-GITHUB_TOKEN = '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'
-GITHUB_USERNAME = 'opefeni'
+GITHUB_TOKEN = 'your-github-token'
+GITHUB_USERNAME = 'your-username'
 GITHUB_REST_API = 'https://api.github.com'
